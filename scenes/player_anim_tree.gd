b03d816660d843918ff07577be330a7df7569681
Ermis <cydisalt@gmail.com>
2024-04-26 15:00:09 +0300

    init

---
+++ scenes/player_anim_tree.gd
@@ -0,0 +1,10 @@
+extends AnimationTree
+
+@export var left = false
+@export var right = true
+@export var changed_facing = false
+@export var running = false
+@export var idling = true
+@export var grounded = true
+@export var small_v_speed = true
+@export var descending = false
